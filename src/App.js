04d6151e43d71f76ff5f9cc31fefd4ee34fb6c09
wataharu-84 <haruki.go84@gmail.com>
2022-09-25 19:19:44 +0900

    06

--- src/App.js
+++ src/App.js
@@ -2,15 +2,27 @@
 
 import * as React from 'react'
 import './App.css'
+import UseState, { useState } from 'react';
 
 /**
- * 
+ *
  * @type {React.FC}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = useState("https://images.dog.ceo/breeds/coonhound/n02089078_237.jpg");
+  var newImg;
+  fetch("https://dog.ceo/api/breeds/image/random")
+    .then(response => response.json())
+    .then(data => newImg = data.message);
+
   return (
-    <div>
-      <h2>Hello, world!</h2>
-    </div>
+    <>
+      <header>
+        Dogアプリ
+      </header>
+      <p>犬の画像を表示するサイトです。</p>
+      <img src={dogUrl}></img>
+      <button onClick={() => setDogUrl(newImg)}>Click here!!</button>
+    </>
   )
 }
